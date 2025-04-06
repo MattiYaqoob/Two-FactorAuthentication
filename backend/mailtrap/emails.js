947bec401b88cf9b6642864568e5d2b6bf81febc
MattiYaqoob <matti.al-hayali@studerande.plushogskolan.se>
2025-04-06 19:47:07 +0200

    the verifyEmail working and added

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,5 +1,5 @@
 import { mailtrapclient, sender } from "./mailTrap.config.js";
-import { VERIFICATION_EMAIL_TEMPLATE} from "./emailTemplates.js";
+import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
 
 
 export const sendVerificationEmail = async (email, verificationToken) => {
@@ -20,3 +20,24 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     throw new Error(`Error sending verification email: ${error.message}`);
   }
 };
+
+export const sendWelcomeEmail = async (email, name) => {
+
+  const recipients = [{ email }]
+
+  try {
+    const response = await mailtrapclient.send({
+      from: sender,
+      to: recipients,
+      template_uuid: "9ba02f22-cd06-4563-9d08-283aa479884a",
+      template_variables: {
+        "company_info_name": "Klara",
+        "name": name
+      }
+    })
+    console.log("Email sent successfully", response)
+  } catch (error) {
+
+    throw new Error(`error sending welcome email:${error}`)
+  }
+}
